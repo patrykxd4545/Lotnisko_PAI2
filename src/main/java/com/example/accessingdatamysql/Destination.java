833b4503625887e9afa8f3aeec051272840f594a
xdawidef <dawmilczarek@gmail.com>
2021-04-08 21:45:39 +0200

    Update

--- src/main/java/com/example/accessingdatamysql/Destination.java
+++ src/main/java/com/example/accessingdatamysql/Destination.java
@@ -8,25 +8,8 @@ public class Destination {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "destination_id")
-    private long destination_id;
-    private String from;
-    private String to;
+    private Long destination_id;
+    private String destination_from;
+    private String getDestination_to;
 
-
-
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
-    public String getTo() {
-        return to;
-    }
-
-    public void setTo(String to) {
-        this.to = to;
-    }
 }
