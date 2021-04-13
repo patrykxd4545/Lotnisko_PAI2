e12a5763a7811f9a223475b469d17031b71dbcf8
Albert <albertcisc98@gmail.com>
2021-04-13 11:18:13 +0200

    Bercik

--- src/main/java/com/example/accessingdatamysql/Ticket.java
+++ src/main/java/com/example/accessingdatamysql/Ticket.java
@@ -10,14 +10,11 @@ public class Ticket {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "ticket_id")
     private Long ticket_id;
-    private Integer order;
+
     private Date flightDate;
     private Integer seat_number;
     private Double cost;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "passanger_id", referencedColumnName = "passanger_id")
-    private Passanger passanger_id;
 
 
 }
